52eeddf5f98f66df112e1c61a08f1df28472c9b2
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-03-15 10:52:21 +1000

    Переименовка

--- Android/app/src/main/java/com/hitim/android/itstime/OnBoardActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/OnBoardActivity.java
@@ -3,11 +3,11 @@ package com.hitim.android.itstime;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OnBoardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_on_board);
     }
 }
