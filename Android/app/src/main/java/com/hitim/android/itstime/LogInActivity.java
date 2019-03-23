32ac639c272b050919d99f77caca260796134fcf
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-03-23 18:46:28 +1000

    Фикс

--- Android/app/src/main/java/com/hitim/android/itstime/LogInActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/LogInActivity.java
@@ -9,5 +9,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
     }
 }
