e8e2a852f43c57c070f66bd283a0ce43e6b479e7
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-03-25 00:43:19 +1000

    Работа Над Toolbar

--- Android/app/src/main/java/com/hitim/android/itstime/SphereActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/SphereActivity.java
@@ -1,13 +1,18 @@
 package com.hitim.android.itstime;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class SphereActivity extends AppCompatActivity {
 
+    public MenuItem searchMenuItem;
     public Toolbar toolbar;
 
     @Override
@@ -15,6 +20,8 @@ public class SphereActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sphere);
         getAllItems();
+        toolbar.setTitle(R.string.sphere);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
     }
 
     private void getAllItems() {
@@ -25,17 +32,36 @@ public class SphereActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.sphere_menu, menu);
+        MenuItem mSearch = menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) mSearch.getActionView();
+        searchView.setQueryHint(getString(R.string.search_text));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                //do the search here
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_search) {
-
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                Intent i = new Intent(SphereActivity.this, ProfileActivity.class);
+                startActivity(i);
+                break;
         }
-        /*if(item.getItemId() == R.id.item){
-            TODO:Сделать функции других элементов
-        }*/
         return true;
     }
 }
