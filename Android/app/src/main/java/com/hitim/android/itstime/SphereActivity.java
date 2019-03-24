fe6f988da0ef03866459bb97e9b3e753640e0100
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-03-24 22:27:04 +1000

    Добавление Toolbar

--- Android/app/src/main/java/com/hitim/android/itstime/SphereActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/SphereActivity.java
@@ -2,12 +2,40 @@ package com.hitim.android.itstime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SphereActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sphere);
+        getAllItems();
+    }
+
+    private void getAllItems() {
+        toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sphere_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_search) {
+            
+        }
+        /*if(item.getItemId() == R.id.item){
+            TODO:Сделать функции других элементов
+        }*/
+        return true;
     }
 }
