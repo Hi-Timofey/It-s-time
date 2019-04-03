ae73c9e772d5217292d144a2961aeea06c304c9c
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-04-03 18:45:52 +1000

    Firebase

--- Android/app/src/main/java/com/hitim/android/itstime/LogInActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/LogInActivity.java
@@ -8,17 +8,33 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogInActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private EditText edLogin, edPass;
     private ImageButton regButton;
+    //Firebase
+    private FirebaseApp mFireBaseApp;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startFireBase();
         setContentView(R.layout.activity_log_in);
         getAlltems();
         regButton.setOnTouchListener(this);
+    }
+    //Обрабтка Firebase
+    private void startFireBase() {
+        mFireBaseApp = FirebaseApp.initializeApp(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
     }
 
     private void getAlltems() {
