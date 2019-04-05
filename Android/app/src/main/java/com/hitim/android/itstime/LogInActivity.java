54a21d08409ed95aebe1d65220b1fffc20308d26
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-04-05 21:37:19 +1000

    TOAST!

--- Android/app/src/main/java/com/hitim/android/itstime/LogInActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/LogInActivity.java
@@ -7,6 +7,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
