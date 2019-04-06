6a9203f34e7c4507841d18fedae248a453e4a680
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-04-07 09:21:28 +1000

    Фиксы

--- Android/app/src/main/java/com/hitim/android/itstime/RegisterActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/RegisterActivity.java
@@ -1,13 +1,28 @@
 package com.hitim.android.itstime;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mailLayout, passLayout;
+    private TextInputEditText edMail, edPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mailLayout = findViewById();
+        passLayout = findViewById();
+        edMail = findViewById();
+        edPass = findViewById();
+
+    }
+
+    public void onRegIn(View view) {
     }
 }
