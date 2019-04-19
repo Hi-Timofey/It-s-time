ec888820b2a0ba4d45db77c841ec041001705967
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-04-19 21:54:29 +1000

    Попытки работы с фрагментами

---
+++ Android/app/src/main/java/com/hitim/android/itstime/ProfileFragment.java
@@ -0,0 +1,25 @@
+package com.hitim.android.itstime;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.profile_layout,container,false);
+
+        return view;
+
+    }
+}
