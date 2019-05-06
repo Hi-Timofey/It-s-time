b80feb9d7d6913e979bf4f4edcd09b8b52e3d2b1
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-05-06 17:29:33 +1000

    Фиксы фиксов

--- Android/app/src/main/java/com/hitim/android/itstime/SphereActivity.java
+++ Android/app/src/main/java/com/hitim/android/itstime/SphereActivity.java
@@ -2,17 +2,17 @@ package com.hitim.android.itstime;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class SphereActivity extends AppCompatActivity {
+public class SphereActivity extends AppCompatActivity{
 
     public com.github.clans.fab.FloatingActionMenu fabMenu;
     public Toolbar toolbar;
@@ -36,23 +36,87 @@ public class SphereActivity extends AppCompatActivity {
         super.onStart();
         sphereFragment = new SphereFragment();
         fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, sphereFragment, getString(R.string.sphere_fragment))
+                .replace(R.id.fragment_container,sphereFragment,getString(R.string.sphere_fragment))
                 .addToBackStack(null)
                 .commit();
     }
 
+<<<<<<< HEAD
+=======
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sphere_menu, menu);
+        searchManager(menu);
+        return true;
+    }
+
+    //Обработка поиска через Toolbar
+    public void searchManager(Menu menu){
+        MenuItem mSearch = menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) mSearch.getActionView();
+        searchView.setQueryHint(getString(R.string.search_text));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //TODO: делать нормальную анимацию для Floating Button когад идет поиск текста
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //floatingActionButton.show(false);
+                //do the search here
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //floatingActionButton.hide(false);
+            }
+        });
+    }
+
+    //Обработка нажатий кнопок на Toolbar'е
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                fragment = new UserProfileFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container,fragment,getString(R.string.user_profile_fragment))
+                        .addToBackStack(null)
+                        .commit();
+                break;
+            case R.id.action_settings:
+                    fragment = new SettingsFragment();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container,fragment,getString(R.string.settings_fragment))
+                        .addToBackStack(null)
+                        .commit();
+                break;
+        }
+        return true;
+    }
+
+
+>>>>>>> parent of 332edc5... Фиксы фиксов
     public void OnFabClick(View view) {
         Fragment fragment;
-        switch (view.getId()) {
+        switch (view.getId()){
             case R.id.create_sphere_fab:
 
                 break;
             case R.id.create_task_fab:
                 fragment = new CreateTaskFragment();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, fragment, getString(R.string.create_task_fragment))
-                        .addToBackStack(null)
-                        .commit();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container,fragment,getString(R.string.create_task_fragment))
+                            .addToBackStack(null)
+                            .commit();
                 break;
         }
     }
