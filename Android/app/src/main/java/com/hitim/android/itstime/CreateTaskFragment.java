f06c4e692a2849f8ec0368d2d701edcdb33e00b8
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-05-08 17:02:37 +1000

    Работаю с новым окном

--- Android/app/src/main/java/com/hitim/android/itstime/CreateTaskFragment.java
+++ Android/app/src/main/java/com/hitim/android/itstime/CreateTaskFragment.java
@@ -5,26 +5,29 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.github.clans.fab.FloatingActionMenu;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class CreateTaskFragment extends Fragment {
+public class CreateTaskFragment extends Fragment implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     private FloatingActionMenu floatingActionMenu;
     private Toolbar toolbar;
     private AppBarLayout l;
+    private TextView deadText, reminderText, tagsText, sphereCardText;
+    private ImageButton deadImg, remindImg, taskIcon;
+    private CheckBox dateBox, remindBox;
+    private CardView sphereCard;
 
     public CreateTaskFragment() {
     }
@@ -39,8 +42,19 @@ public class CreateTaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_create_task, container, false);
+        reminderText = v.findViewById(R.id.userToDoRemindMeTextView);
+        deadText = v.findViewById(R.id.deadline_text_view);
+        dateBox = v.findViewById(R.id.date_check_box);
+        remindBox = v.findViewById(R.id.reminder_check_box);
         floatingActionMenu = getActivity().findViewById(R.id.floating_button_menu);
-        toolbar = (Toolbar) ((AppCompatActivity) getActivity()).findViewById(R.id.tool_bar);
+        toolbar = getActivity().findViewById(R.id.tool_bar);
+        sphereCard = v.findViewById(R.id.sphere_mini_card_view);
+        sphereCardText = v.findViewById(R.id.mini_card_text);
+
+
+        sphereCard.setOnClickListener(this);
+        dateBox.setOnCheckedChangeListener(this);
+        remindBox.setOnCheckedChangeListener(this);
         toolbar.setTitle(getString(R.string.create_task));
         toolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -73,6 +87,21 @@ public class CreateTaskFragment extends Fragment {
         l.setElevation(2);
         floatingActionMenu.setVisibility(View.VISIBLE);
         floatingActionMenu.showMenuButton(true);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.date_check_box:
+                break;
+            case R.id.reminder_check_box:
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
