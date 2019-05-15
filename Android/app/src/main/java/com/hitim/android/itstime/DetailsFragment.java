f3275cc298b077daf72b034a5402ac69a2fe8194
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-05-15 23:13:10 +1000

    Создал фрагмент со списками

---
+++ Android/app/src/main/java/com/hitim/android/itstime/DetailsFragment.java
@@ -0,0 +1,30 @@
+package com.hitim.android.itstime;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class DetailsFragment extends ListFragment {
+
+    private ListView listView;
+
+    public DetailsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_details, container, false);
+        listView = v.findViewById(R.id.task_list_view);
+        return v;
+    }
+
+}
