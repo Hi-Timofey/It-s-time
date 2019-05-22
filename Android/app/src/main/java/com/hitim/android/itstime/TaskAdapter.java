f1e9186dfa5334ceecdb3f089217dbf587665922
Hi-Timofey <43324422+Hi-Timofey@users.noreply.github.com>
2019-05-22 23:31:24 +1000

    Revert "Фиксы"
    This reverts commit 5eeb6f96f308759e32669c0cfa74c94d8015b9ac.

--- Android/app/src/main/java/com/hitim/android/itstime/TaskAdapter.java
+++ Android/app/src/main/java/com/hitim/android/itstime/TaskAdapter.java
@@ -8,13 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.joooonho.SelectableRoundedImageView;
 
 import java.util.List;
 
-/**
- * {@link TaskAdapter} - адаптер для списков задач
+/**{@link TaskAdapter} - адаптер для списков задач
  * Благодаря нему выносится большее количество информации о {@link Task}
  * на экран, чем в обычном списке
  */
@@ -48,13 +48,14 @@ public class TaskAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
 
-        if (convertView == null) {
-            LayoutInflater inf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if(convertView == null){
+            LayoutInflater inf = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inf.inflate(R.layout.task_layout, null);
             holder = new ViewHolder(convertView);
             convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
+        }
+        else {
+            holder = (ViewHolder)convertView.getTag();
         }
 
         Task task = taskArrayList.get(position);
@@ -63,26 +64,11 @@ public class TaskAdapter extends BaseAdapter {
         image.setColorFilter(color, PorterDuff.Mode.XOR);
 
         holder.name.setText(task.getName());
+        holder.sphere.setText(task.getSphere());
         holder.deadline.setText(task.getDatePicked().toString());
         holder.priority.setText(String.valueOf(task.getPriority()));
         holder.imageView.setImageDrawable(image);
         holder.imageView.setBorderColor(color);
-        int resId = 0;
-        switch (task.getSphere()) {
-            case "Work":
-                resId = R.string.work;
-                break;
-            case "Health":
-                resId = R.string.health;
-                break;
-            case "Routine":
-                resId = R.string.routine;
-                break;
-            case "Yourself":
-                resId = R.string.yourself;
-                break;
-        }
-        holder.sphere.setText(context.getString(resId));
         /*holder.imageView.setOnClickListener(v -> {
             Toast.makeText(context,String.valueOf(task.getId()),Toast.LENGTH_SHORT).show();
              AsyncWorker worker = new AsyncWorker();
@@ -96,14 +82,14 @@ public class TaskAdapter extends BaseAdapter {
         return convertView;
     }
 
-    private static class ViewHolder {
+    private static class ViewHolder{
         public TextView name;
         public TextView sphere;
         public TextView deadline;
         public SelectableRoundedImageView imageView;
         public TextView priority;
 
-        public ViewHolder(View v) {
+        public ViewHolder(View v){
 
             this.name = v.findViewById(R.id.task_layout_name);
             this.sphere = v.findViewById(R.id.task_layout_sphere);
