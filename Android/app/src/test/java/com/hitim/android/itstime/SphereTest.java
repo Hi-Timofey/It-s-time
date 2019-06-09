0f96b167233dc1e45832f6a7e9fe7f840557fb26
Hi-Timofey <Katkov0103@yandex.ru>
2019-06-09 16:08:56 +1000

    Tests

---
+++ Android/app/src/test/java/com/hitim/android/itstime/SphereTest.java
@@ -0,0 +1,41 @@
+package com.hitim.android.itstime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class SphereTest {
+
+
+    private String name;
+    private Integer drawableId;
+    private Integer iconId;
+    private Sphere defaultSphereWork;
+    private ArrayList<Sphere> defaultSpheres;
+
+    @Before
+    public void init() throws Exception {
+
+        name = "Work";
+        drawableId = R.drawable.work_background;
+        iconId = R.drawable.ic_work;
+        defaultSphereWork = new Sphere(name, drawableId, iconId);
+
+        defaultSpheres = new ArrayList<>();
+        defaultSpheres.add(new Sphere("All task's", R.drawable.all_tasks_background, R.drawable.ic_arrow_forward));
+        defaultSpheres.add(new Sphere("Work", R.drawable.work_background, R.drawable.ic_work));
+        defaultSpheres.add(new Sphere("Health", R.drawable.health_background, R.drawable.ic_heart));
+        defaultSpheres.add(new Sphere("Routine", R.drawable.routine_background, R.drawable.ic_routine));
+        defaultSpheres.add(new Sphere("Yourself", R.drawable.yourself_background, R.drawable.ic_nature_people_white_24dp));
+    }
+
+    @Test
+    public void testSphereClass() throws Exception {
+        assertEquals("Sphere name exception", name, defaultSphereWork.getName());
+        assertEquals("Sphere drawable exception", drawableId, defaultSphereWork.getDrawableId());
+        assertEquals("Sphere icon exception", iconId, defaultSphereWork.getIconId());
+    }
+}
