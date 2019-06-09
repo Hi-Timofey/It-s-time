52aab9ff4a5dea04054ce650dd30ae022d59f621
Hi-Timofey <Katkov0103@yandex.ru>
2019-06-09 15:57:07 +1000

    tests

--- Android/app/src/test/java/com/hitim/android/itstime/DatePickedTest.java
+++ Android/app/src/test/java/com/hitim/android/itstime/DatePickedTest.java
@@ -1,21 +1,37 @@
 package com.hitim.android.itstime;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertTrue;
 
 public class DatePickedTest {
-    private int hour = 6;
-    private int minutes = 45;
-    private int year = 2019;
-    private int month = 5;
-    private int day = 30;
-    private DatePicked defaultDatePicked = new DatePicked(hour,minutes,year,month,day);
+    private int hour;
+    private int minutes;
+    private int year;
+    private int month;
+    private int day;
+    private DatePicked defaultDatePicked, nullable;
+    private DatePicked onlyTime;
+    private DatePicked onlyDate;
+
+    @Before
+    public void init() throws Exception {
+        hour = 6;
+        minutes = 45;
+        year = 2019;
+        month = 5;
+        day = 30;
+        defaultDatePicked = new DatePicked(hour,minutes,year,month,day);
+        nullable = new DatePicked(hour,minutes,year,month,day);
+        onlyDate = new DatePicked(year,month,day);
+        onlyTime = new DatePicked(hour,minutes);
+
+        nullable.resetAll();
+    }
 
     @Test
-    public void testDatePickedClass(){
+    public void testDatePickedClass() throws Exception{
         assertEquals("Exception in Date class (HOUR)",hour,defaultDatePicked.getHour());
         assertEquals("Exception in Date class (MINUTES)",minutes,defaultDatePicked.getMinutes());
         assertEquals("Exception in Date class (YEAR)",year,defaultDatePicked.getYear());
@@ -23,4 +39,11 @@ public class DatePickedTest {
         assertEquals("Exception in Date class (DAY)",day,defaultDatePicked.getDay());
     }
 
+    @Test
+    public void testDateType() throws Exception{
+        assertEquals("Date type exception",DatePicked.TIME_DATE,defaultDatePicked.getType());
+        assertEquals("Date type exception",DatePicked.DATE_ONLY,onlyDate.getType());
+        assertEquals("Date type exception",DatePicked.TIME_ONLY,onlyTime.getType());
+        assertEquals("Date type exception",DatePicked.NULL_TIME,nullable.getType());
+    }
 }
--- Android/app/src/test/java/com/hitim/android/itstime/TaskClassTest.java
+++ Android/app/src/test/java/com/hitim/android/itstime/TaskClassTest.java
@@ -2,13 +2,13 @@ package com.hitim.android.itstime;
 
 import android.graphics.Color;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -18,31 +18,55 @@ import static org.junit.Assert.assertTrue;
  */
 public class TaskClassTest {
 
-    private String name = "Test yourself";
-    private String description = "Description";
-    private DatePicked datePicked = new DatePicked(6,45,2019,6,1);
-    private DatePicked neededTime = new DatePicked(3,15,2019,5,1);
-    private String sphereDef = "Work";
-    private String[] spheres = {"Work", "Yourself", "Routine", "Health"};
-    private int color = Color.BLUE;
-    private Task tsk = new Task(name,description,datePicked, sphereDef,color,neededTime,1);
+    private String name;
+    private String description;
+    private DatePicked datePicked;
+    private DatePicked neededTime;
+    private String sphereDef;
+    private String[] spheres;
+    private int color;
+    private Task tsk;
+    private ArrayList<Task> taskArrayList;
+
+    @Before
+    public void init() throws Exception{
+        name = "Test yourself";
+        description = "Description";
+        datePicked = new DatePicked(6, 45, 2019, 6, 1);
+        neededTime = new DatePicked(3, 15, 2019, 5, 1);
+        sphereDef = "Work";
+        spheres = new String[]{"Work", "Yourself", "Routine", "Health"};
+        color = Color.BLUE;
+        tsk = new Task(name, description, datePicked, sphereDef, color, neededTime, 1);
+
+        taskArrayList = new ArrayList<>();
+        Task taskWork = new Task(name, description, datePicked, "Work", color, neededTime, 1);
+        Task taskHealth = new Task(name, description, datePicked, "Health", color, neededTime, 1);
+        Task taskRoutine = new Task(name, description, datePicked, "Routine", color, neededTime, 1);
+        Task taskYourself = new Task(name, description, datePicked, "Yourself", color, neededTime, 1);
+        taskArrayList.add(taskYourself);
+        taskArrayList.add(taskHealth);
+        taskArrayList.add(taskWork);
+        taskArrayList.add(taskRoutine);
+    }
+
 
     @Test
-    public void testTaskClass() {
-        assertEquals("Task name exception",name,tsk.getName());
-        assertEquals("Task description exception",description, tsk.getDescription());
-        assertEquals("Task date(deadline) exception",datePicked,tsk.getDatePicked());
-        assertEquals("Task date(start) exception",neededTime,tsk.getNeededTimePicked());
-        assertEquals("Task sphereDef exception", sphereDef,tsk.getSphere());
-        assertEquals("Task color exception",color,tsk.getColor());
+    public void testTaskClass() throws Exception{
+        assertEquals("Task name exception", name, tsk.getName());
+        assertEquals("Task description exception", description, tsk.getDescription());
+        assertEquals("Task date(deadline) exception", datePicked, tsk.getDatePicked());
+        assertEquals("Task date(start) exception", neededTime, tsk.getNeededTimePicked());
+        assertEquals("Task sphereDef exception", sphereDef, tsk.getSphere());
+        assertEquals("Task color exception", color, tsk.getColor());
     }
 
     @Test
-    public void testTaskDate(){
+    public void testTaskDate() throws Exception{
         Calendar ndTm = neededTime.getCalendarFormat();
         Calendar dtTm = datePicked.getCalendarFormat();
         assertEquals("Task date(start) exception  with Calendar format",
-                ndTm.getTime().toString() ,
+                ndTm.getTime().toString(),
                 tsk.getNeededTimePicked().getCalendarFormat().getTime().toString()
         );
         assertEquals("Task date(deadline) exception with Calendar format",
@@ -52,19 +76,11 @@ public class TaskClassTest {
     }
 
     @Test
-    public void testTaskSphere(){
-        Task taskWork = new Task(name,description,datePicked, "Work",color,neededTime,1);
-        Task taskHealth = new Task(name,description,datePicked, "Health",color,neededTime,1);
-        Task taskRoutine = new Task(name,description,datePicked, "Routine",color,neededTime,1);
-        Task taskYourself = new Task(name,description,datePicked, "Yourself",color,neededTime,1);
-        ArrayList<Task> taskArrayList = new ArrayList<>();
-        taskArrayList.add(taskYourself);
-        taskArrayList.add(taskHealth);
-        taskArrayList.add(taskWork);
-        taskArrayList.add(taskRoutine);
-        for (int i = 0;i < 4;i++){
+    public void testTaskSphere() throws Exception{
+
+        for (int i = 0; i < 4; i++) {
             boolean correct = false;
-            for(int j = 0;j < 4;j++){
+            for (int j = 0; j < 4; j++) {
                 String tmp = taskArrayList.get(i).getSphere();
                 correct = tmp.equals(spheres[j]);
                 if (correct) break;
